ca85f9736b5f95a3cec1075bb45b8fee6fef7af3
roydon <3133010060@qq.com>
2024-05-19 18:38:31 +0800

    优化

--- src/Layout/user/VideoViewHistory.vue
+++ src/Layout/user/VideoViewHistory.vue
@@ -11,50 +11,6 @@ export default {
   components: {MapView, MediaCardWaterfall},
   data() {
     return {
-      mediaList: [
-        {
-          imgSrc: "https://tse3-mm.cn.bing.net/th/id/OIP-C.FaNkJVeiGCXW6YolPOvbQAHaHa?w=220&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          avatar: "https://tse3-mm.cn.bing.net/th/id/OIP-C.FaNkJVeiGCXW6YolPOvbQAHaHa?w=220&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          user: "Lewis",
-          vote_num: 999,
-          title: "Noice cancelling is a secret weapon",
-        },
-        {
-          imgSrc: "https://tse4-mm.cn.bing.net/th/id/OIP-C.-8sTBQWU55DOkQDjds4BrAHaKk?w=154&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          avatar: "https://tse4-mm.cn.bing.net/th/id/OIP-C.-8sTBQWU55DOkQDjds4BrAHaKk?w=154&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          user: "Lewis",
-          vote_num: 999,
-          title: "Work from home is so boring",
-        },
-        {
-          imgSrc: "https://tse3-mm.cn.bing.net/th/id/OIP-C.FaNkJVeiGCXW6YolPOvbQAHaHa?w=220&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          avatar: "https://tse3-mm.cn.bing.net/th/id/OIP-C.FaNkJVeiGCXW6YolPOvbQAHaHa?w=220&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          user: "Lewis",
-          vote_num: 999,
-          title: "Noice cancelling is a secret weapon",
-        },
-        {
-          imgSrc: "https://tse4-mm.cn.bing.net/th/id/OIP-C.-8sTBQWU55DOkQDjds4BrAHaKk?w=154&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          avatar: "https://tse4-mm.cn.bing.net/th/id/OIP-C.-8sTBQWU55DOkQDjds4BrAHaKk?w=154&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          user: "Lewis",
-          vote_num: 999,
-          title: "Work from home is so boring",
-        },
-        {
-          imgSrc: "https://tse3-mm.cn.bing.net/th/id/OIP-C.FaNkJVeiGCXW6YolPOvbQAHaHa?w=220&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          avatar: "https://tse3-mm.cn.bing.net/th/id/OIP-C.FaNkJVeiGCXW6YolPOvbQAHaHa?w=220&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          user: "Lewis",
-          vote_num: 999,
-          title: "Noice cancelling is a secret weapon",
-        },
-        {
-          imgSrc: "https://tse4-mm.cn.bing.net/th/id/OIP-C.-8sTBQWU55DOkQDjds4BrAHaKk?w=154&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          avatar: "https://tse4-mm.cn.bing.net/th/id/OIP-C.-8sTBQWU55DOkQDjds4BrAHaKk?w=154&h=220&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-          user: "Lewis",
-          vote_num: 999,
-          title: "Work from home is so boring",
-        },
-      ]
     }
   },
   created() {
